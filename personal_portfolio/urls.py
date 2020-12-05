b8f81e788d53bacb0c4e3daaf63705a0fa93e9c0
NasrShamalkh <naaaa9sr-@hotmail.com>
2020-12-05 22:37:44 +0200

    adding never_cache

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', never_cache(TemplateView.as_view(template_name="index.html"))),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
